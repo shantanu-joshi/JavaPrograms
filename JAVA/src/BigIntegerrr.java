3e724d024dda04d92303ac8f085bce26f7a5f210
Shantanu  Joshi <44423695+shantanu-joshi@users.noreply.github.com>
2021-12-15 20:32:17 +0530

    Update BigIntegerrr.java
    added comments

--- JAVA/src/BigIntegerrr.java
+++ JAVA/src/BigIntegerrr.java
@@ -1,15 +1,15 @@
-import java.math.BigInteger;
+import java.math.BigInteger;     //For inheriting BigInteger classes
 import java.util.Scanner;
 
 public class BigIntegerrr {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);  
 		
-		BigInteger n = sc.nextBigInteger();
+		BigInteger n = sc.nextBigInteger();   //For taking bigInter as input by the system 
 		
-		System.out.println(n.isProbablePrime(100)?"prime":"not Prime");
+		System.out.println(n.isProbablePrime(100)?"prime":"not Prime");  //checking prime numbers with in built bigInterger method that is isProbable prime
 		
 		
 		
